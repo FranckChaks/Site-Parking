c1f8702e12358ad5ac6524c641175cede76168ab
FranckChaks <franck1891@hotmail.fr>
2017-10-06 17:43:48 +0200

    Ajout footer & header

---
+++ view/header.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>Parking</title>
+	<link rel="stylesheet" href="css/style2.css"/>
+	<link rel="stylesheet" href="css/bootstrap.css"/>
+		<meta charset="utf8"/>
+</head>
+<header>
+    <div class="row">
+       <div class="col-xs-12 col-lg-12 entete">
+            <nav>
+                <ul>
+                   <li class="col-xs-2 col-md-2 liste"><span class="glyphicon glyphicon-off"></span> Connexion</li>
+                   <li class="col-xs-2 col-md-2 liste"><span class="glyphicon glyphicon-pencil"> </span> Inscription</li>
+                </ul>
+            </nav>
+        </div>
+     </div>
+        
+     <div class="row">
+        <div class="col-xs-12 col-md-12 titre">
+               <p class="titre_parking">Parking.yeah</p>
+        </div>
+     </div>
+</header>
+<body>
+        
