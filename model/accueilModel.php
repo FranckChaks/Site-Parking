999116bdda333c29e173e378537043809a59cccf
FranckChaks <franck1891@hotmail.fr>
2017-10-06 17:46:09 +0200

    Ajout footer & header

--- model/accueilModel.php
+++ model/accueilModel.php
@@ -1,32 +1,7 @@
 <?php
   function signIn($nom, $prenom $email, $mdp, $confirm, $bdd)
   {
-    if(empty($nom) OR empty($prenom) OR empty($email) OR empty($mdp) OR empty($confirm))
-    {
-      $error = "Veuillez remplir tous les champs du formulaire";
-      return $error;
-    }
-    else
-    {
-      if($mdp != $confirm)
-      {
-        $error = "Les mots de passe ne correspondent pas";
-        return $error;
-      }
-      else
-      {
-        $req = $bdd->prepare("SELECT * FROM user WHERE login = :login");
-        $req->bindValue('login', $login, PDO::PARAM_STR);
-        $req->execute();
-        $rep = $req->fetch();
-
-        if($rep)
-        {
-          $error = "Ce login est déja utilisé";
-          return $error;
-        }
-        else
-        {
+    
           $crypt = sha1($mdp);
 
           $req = $bdd->prepare("INSERT INTO user (id_u, nom, prenom, email, mdp, lvl) VALUES (NULL, :nom, :prenom, :email, :mdp, 1)");
@@ -44,22 +19,11 @@
           $_SESSION['email'] = $rep['email'];
           $_SESSION['lvl'] = $rep['lvl'];
 
-          header("Location:index.php");
-        }
-      }
-    }
   }
 
 
   function login($email, $mdp, $bdd)
   {
-    if(empty($email) OR empty($mdp))
-    {
-      $error = "<div class =' alert alert-warning error col-lg-3'>Veuillez remplir tous les champs du formulaire.</div>";
-      return $error;
-    }
-    else
-    {
       $req = $bdd -> prepare("SELECT * FROM user WHERE email = :email AND mdp = :mdp");
       $req->execute(array(
         'email'=>$email,
