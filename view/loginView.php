69b92fac3aa1fefb789a9749d302986364413e0a
FranckChaks <franck1891@hotmail.fr>
2017-10-13 15:12:35 +0200

    Ajout module signUp et Login

---
+++ view/loginView.php
@@ -0,0 +1,31 @@
+<button type="button" class="bouton" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-off"></span> Connexion</button>
+       
+
+<!-- Pop-up Connexion-->
+        <div class="modal fade color-black" id="myModal" role="dialog">
+            <div class="modal-dialog">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header titre_co">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title titre_co">Connexion à Parking.yeah !</h4> </div>
+                    <div class="modal-body">
+                        <!--------------FORMULAIRE----------------->
+                        <form method="post" action="#">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Adresse E-mail</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" name="email"> </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Mot de passe</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe" name="mdp"> </div>
+                            <button type="submit" class="btn btn-success" name="submit">Se connecter</button>
+                        </form>
+                        <!----------------------------------------->
+                    </div>
+                    <div class="modal-footer titre_co">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!----------------------------->
