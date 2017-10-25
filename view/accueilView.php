d0c742279c0a63528989ae25614105860ac56b3a
FranckChaks <franck1891@hotmail.fr>
2017-10-25 22:40:04 +0200

    Re-design accueil

--- view/accueilView.php
+++ view/accueilView.php
@@ -1,6 +1,6 @@
 <div class="row">
-    <div class="col-xs-12 col-lg-12 padding_0">
-        <img class="img_responsive" width="100% 100%" src="css/item1.jpg">
+    <div class="col-xs-12 col-md-12">
+        <img width="100% 100%" src="css/item1.jpg">
     </div>
 </div>
 
--- view/header.php
+++ view/header.php
@@ -1,17 +1,8 @@
-<html>
 
-<head>
-    <title>Parking</title>
-    <link rel="stylesheet" href="css/style2.css" />
-    <link rel="stylesheet" href="css/bootstrap.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <meta charset="utf8" />
-</head>
 
 
     <header>
-    <div class="container-fluid">
+    
         <div class="row">
             <div class="col-xs-12 col-lg-12 entete">
                 <!----------- ENTETE ---------------->
@@ -48,12 +39,17 @@
         </div>
         
 
-        </div>
-    </header>
-    <div class="row">
+ 
+    
+    <div class="row top0">
         <div class="col-xs-12 col-md-12 titre">
             <a class="href" href="index.php"><p class="titre_parking">Parking.yeah</p></a>
         </div>
     </div>
-    
+    </header>
+    <div class="row">
+        <div class="col-xs-12 col-md-12 entete2">
+            
+        </div>
+    </div>
 <body>
